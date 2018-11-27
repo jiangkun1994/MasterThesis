4488d40c3fe141a8a37d4b3af99b00d7e8eecb57
jiangkun1994 <jiangkun1994@163.com>
2018-11-27 12:13:41 +0100

    add shell

---
+++ installNvidia.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Checking for CUDA and installing."
+# Check for CUDA and try to install.
+if ! dpkg-query -W cuda; then
+  # The 16.04 installer works with 16.10.
+  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+  sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+  sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+  sudo apt-get update
+  sudo apt-get install cuda-8.0
+fi
+echo 'export CUDA_HOME=/usr/local/cuda-8.0' >> ~/.bashrc
+echo 'export PATH=$PATH:$CUDA_HOME/bin' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH=$CUDA_HOME/lib64' >> ~/.bashrc
+source ~/.bashrc
